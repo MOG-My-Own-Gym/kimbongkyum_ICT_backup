fec5fe1592294e15557b857b3fca3e3109cdbcb7
kimbongkum <qkdlzld80@naver.com>
2025-07-06 23:57:18 +0900

    테스트용

--- MOG-Front/src/App.jsx
+++ MOG-Front/src/App.jsx
@@ -17,22 +17,104 @@ import LoginPage from './pages/Login/LoginPage';
 import Social from './pages/Social/Social';
 import MyPage from './pages/Mypage/MyPage';
 
+const initRoutineData={routine1:{
+    "name": "routineisgood",
+    "page": "",
+    "user": "kim",
+    "state": [
+        {
+            "id": 1,
+            "names": "3/4 Sit-Up",
+            "category": "strength",
+            "equipment": "body only",
+            "force": "pull",
+            "level": "beginner",
+            "mechanic": "compound",
+            "instructions": [
+                "Lie down on the floor and secure your feet. Your legs should be bent at the knees.",
+                "Place your hands behind or to the side of your head. You will begin with your back on the ground. This will be your starting position.",
+                "Flex your hips and spine to raise your torso toward your knees.",
+                "At the top of the contraction your torso should be perpendicular to the ground. Reverse the motion, going only ¾ of the way down.",
+                "Repeat for the recommended amount of repetitions."
+            ],
+            "primaryMuscles": "abdominals",
+            "imgfile": "https://raw.githubusercontent.com/kimbongkum/ict4e/master/exercises/3_4_Sit-Up/images/0.jpg"
+        },
+        {
+            "id": 1,
+            "names": "3/4 Sit-Up",
+            "category": "strength",
+            "equipment": "body only",
+            "force": "pull",
+            "level": "beginner",
+            "mechanic": "compound",
+            "instructions": [
+                "Lie down on the floor and secure your feet. Your legs should be bent at the knees.",
+                "Place your hands behind or to the side of your head. You will begin with your back on the ground. This will be your starting position.",
+                "Flex your hips and spine to raise your torso toward your knees.",
+                "At the top of the contraction your torso should be perpendicular to the ground. Reverse the motion, going only ¾ of the way down.",
+                "Repeat for the recommended amount of repetitions."
+            ],
+            "primaryMuscles": "abdominals",
+            "imgfile": "https://raw.githubusercontent.com/kimbongkum/ict4e/master/exercises/3_4_Sit-Up/images/0.jpg"
+        },
+        {
+            "id": 1,
+            "names": "3/4 Sit-Up",
+            "category": "strength",
+            "equipment": "body only",
+            "force": "pull",
+            "level": "beginner",
+            "mechanic": "compound",
+            "instructions": [
+                "Lie down on the floor and secure your feet. Your legs should be bent at the knees.",
+                "Place your hands behind or to the side of your head. You will begin with your back on the ground. This will be your starting position.",
+                "Flex your hips and spine to raise your torso toward your knees.",
+                "At the top of the contraction your torso should be perpendicular to the ground. Reverse the motion, going only ¾ of the way down.",
+                "Repeat for the recommended amount of repetitions."
+            ],
+            "primaryMuscles": "abdominals",
+            "imgfile": "https://raw.githubusercontent.com/kimbongkum/ict4e/master/exercises/3_4_Sit-Up/images/0.jpg"
+        },
+        {
+            "id": 1,
+            "names": "3/4 Sit-Up",
+            "category": "strength",
+            "equipment": "body only",
+            "force": "pull",
+            "level": "beginner",
+            "mechanic": "compound",
+            "instructions": [
+                "Lie down on the floor and secure your feet. Your legs should be bent at the knees.",
+                "Place your hands behind or to the side of your head. You will begin with your back on the ground. This will be your starting position.",
+                "Flex your hips and spine to raise your torso toward your knees.",
+                "At the top of the contraction your torso should be perpendicular to the ground. Reverse the motion, going only ¾ of the way down.",
+                "Repeat for the recommended amount of repetitions."
+            ],
+            "primaryMuscles": "abdominals",
+            "imgfile": "https://raw.githubusercontent.com/kimbongkum/ict4e/master/exercises/3_4_Sit-Up/images/0.jpg"
+        }
+      ]
+}}
+
 function App() {
   const { toast, dispatch } = useContext(ToastContext);
+  const [useRoutineData,setRoutineData] = useState(initRoutineData);
   useEffect(() => {
     if (toast.isToast) {
       setTimeout(() => {
         dispatch('HIDE_TOAST');
       }, 2000);
     }
+    console.log('useRoutineData:',useRoutineData);
   }, [toast]);
   return (
     <div style={{ padding: '5em 0 0 0' }}>
       <GNB />
-      <Routes>
+      <Routes context={useRoutineData}>
         <Route path="/" element={<Home />}></Route>
         <Route path="/home" element={<SelectMainpage />}></Route>
-        <Route path="/select" element={<CategoryPage />}></Route>
+        <Route path="/select" element={<CategoryPage useRoutineData={useRoutineData.routine1} setRoutineData = {setRoutineData} />}></Route>
         <Route path="/routine" element={<RoutinePage />}></Route>
         <Route path="/runningroutine" element={<RunningRoutinePage />}></Route>
         <Route path="/routineresult" element={<RoutineResultPage />}></Route>
--- MOG-Front/src/components/mainpage/RoutinePage.jsx
+++ MOG-Front/src/components/mainpage/RoutinePage.jsx
@@ -1,30 +1,33 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "../../assets/bootstrap/css/mainpage.module.css";
 import "../../assets/bootstrap/css/bootstrap.css";
+import { useEffect, useRef, useState } from "react";
 
 export default function RoutinePage(){
     const navigate = useNavigate();
+    const [initMakeRoutine, setMakeRoutine] = useState([]);
+    const location = useLocation();
+    const makeRoutineBoxRef = useRef();
+    useEffect(()=>{
+        setMakeRoutine([...location.state]);
+    },[])
+    console.log('sdfsdfsdf',location);
     return<>
         <div className={"container mt-5 p-3"}></div>
-        <button className={`btn btn-lg btn-primary`} type="button" onClick={()=>navigate(-1)}>뒤로가기</button>
+        <button className={`btn btn-lg btn-primary`} type="button" onClick={()=>navigate('/home')}>뒤로가기</button>
         <div className={`${styles.mainpage} container mt-0 p-0`}>
-                <div className={"container mt-0 p-0"}>
-                    <button className={`${styles.containers} btn btn-lg btn-primary`} type="button" onClick={()=>navigate("/runningroutine")}>
-                        운동1
-                    </button>
-                    <button className={`${styles.containers} btn btn-lg btn-primary`} type="button" onClick={()=>navigate("/runningroutine")}>
-                        운동2
-                    </button>
-                    <button className={`${styles.containers} btn btn-lg btn-primary`} type="button" onClick={()=>navigate("/runningroutine")}>
-                        운동3
-                    </button>
-                    <button className={`${styles.containers} btn btn-lg btn-primary`} type="button" onClick={()=>navigate("/runningroutine")}>
-                        운동4
-                    </button>
-                    <button className={`${styles.containers} btn btn-lg btn-primary`} type="button" onClick={()=>navigate("/runningroutine")}>
-                        운동5
-                    </button>
-                       
+                <div ref={makeRoutineBoxRef} className={"container mt-0 p-0"}>
+                    {
+                    initMakeRoutine.map((item,index)=>
+                    <button key={index} className="btn btn-lg btn-dark"  id={item.names}
+                            style={{width:'100%', fontSize:'25px', textAlign:'left',display:'flex'}}  
+                            type="button" onClick={e=>saveRoutineButton(e)}>
+                            <img alt={item.imgfile} style={{width:'100px'}} src={item.imgfile}/>
+                            {item.names}
+                            <span id={item.names+'Span'} style={{marginLeft:'auto'}}><a href="#">...</a></span>
+                       </button>
+                    )
+                    }             
                 </div>
             <div className={`${styles.dummyContainers} p-5 mt-4`}></div>
             <footer className={`${styles.flexButton}`}>
